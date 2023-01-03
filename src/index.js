5ab3b6de78da5659c64aeaa50343d70d1c53ee27
Josiah Storrie <jpstorrie@gmail.com>
2023-01-03 11:19:35 -0700

    added working state selector

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 //Javascript code file
 const stateSelect = document.querySelector('#state-slct')
-document.querySelector('#search-btn').addEventListener('click', (e)=> {
-    fetch(`https://api.openbrewerydb.org/breweries`)
+const typeSelect = document.querySelector('#search-by-type')
+const resultsDiv = document.querySelector(`#results-div`)
 
-    console.log(stateSelect.value)
+document.querySelector('#search-btn').addEventListener('click', (e)=> {
+    resultsDiv.innerHTML = ""
+    fetch(`http://localhost:3000/breweries`)
+    .then(result => result.json())
+    .then(data => {
+        data.forEach(item => {
+            
+            if(stateSelect.value === item.state || stateSelect.value === `Select State`){
+                const p = document.createElement('p')
+                p.textContent = item.name
+                resultsDiv.append(p)
+            }
+            // if(typeSelect = item.brewery_type){
+
+            // }
+
+        
+    })
+    })
 
 });
