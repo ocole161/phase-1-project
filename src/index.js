2bb2a45152c0c3a885c4399ed449a0e8a48b771f
Josiah Storrie <jpstorrie@gmail.com>
2023-01-03 10:48:25 -0700

    added button listener and db.json file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 //Javascript code file
+const stateSelect = document.querySelector('#state-slct')
+document.querySelector('#search-btn').addEventListener('click', (e)=> {
+    fetch(`https://api.openbrewerydb.org/breweries`)
+
+    console.log(stateSelect.value)
+
+});
