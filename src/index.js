5fd28e436507796e2afc2ccc9580a82173308d42
Owen Cole <ocole161@gmail.com>
2023-01-04 11:45:44 -0700

    added edit form

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const nameSearch = document.querySelector(`#search-bar`)
 const resultsDiv = document.querySelector(`#results-div`)
 const randomDiv = document.querySelector(`#random-div`)
 
-
+// Build list of results
 function renderResults(){
     resultsDiv.innerHTML = ""
     fetch(`http://localhost:3000/breweries`)
@@ -23,56 +23,97 @@ function renderResults(){
     Brewery Type: ${item.brewery_type.charAt(0).toUpperCase() + item.brewery_type.slice(1)}
     ${addressCheck}
     `
-    const webLink = document.createElement('a')
-    webLink.href = `${item.website_url}`
-    webLink.target = '_blank'
-    webLink.textContent = `Website\n`
-    
-    const editBtn = document.createElement('button')
-    editBtn.textContent = 'Edit'
-    
+                const webLink = document.createElement('a')
+                webLink.href = `${item.website_url}`
+                webLink.target = '_blank'
+                webLink.textContent = `Website\n`
+                
+                // Add edit button
+                const editBtn = document.createElement('button')
+                editBtn.textContent = 'Edit'
+                
+                // Only add link if website exists
+                if(item.website_url !== ``){
+                    h5.append(webLink)
+                    editBtn.className = 'editBtn'
+                }
 
-    if(item.website_url !== ``){
-    h5.append(webLink)
-    editBtn.className = 'editBtn'
-    }
+                h5.append(editBtn)
 
-    h5.append(editBtn)
+                // Create edit form
+                function renderForm () {
+                    h5.innerHTML = ''
+                    const nameEdit = document.createElement('input')
+                    const stateEdit = document.createElement('input')
+                    const streetEdit = document.createElement('input')
+                    const ad2Edit = document.createElement('input')
+                    const ad3Edit = document.createElement('input')
+                    const urlEdit = document.createElement('input')
+                    const submitEdit = document.createElement('button')
+                    const deleteBtn = document.createElement('button')
+                    nameEdit.value = item.name
+                    stateEdit.value = item.state
+                    streetEdit.value = item.street
+                    ad2Edit.value = item.address_2
+                    ad3Edit.value = item.address_3
+                    urlEdit.value = item.website_url
+                    submitEdit.textContent = 'Submit'
+                    deleteBtn.textContent = 'Delete'
+                    h5.append(nameEdit)
+                    h5.append(stateEdit)
+                    h5.append(streetEdit)
+                    h5.append(ad2Edit)
+                    h5.append(ad3Edit)
+                    h5.append(urlEdit)
+                    h5.append(submitEdit)
+                    submitEdit.addEventListener('click', () =>
+                        fetch(`http://localhost:3000/breweries/${breweries.}`, {
+                            method: 'PATCH',
+                            headers:{
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify(item)
+                        })
+                        .then(res => res.json())
+                        
+                    )
 
-    editBtn.addEventListener('click', renderForm)
+                    deleteBtn.addEventListener('click', () =>
+                        fetch(`http://localhost:3000/breweries`)
+                    
+                    )
+                    }
 
-        h5.className = `hidden`
-        p.append(h5)       
-        p.addEventListener('mouseover', () =>{
-        h5.className = ``})
-        p.addEventListener('mouseout', () =>{
-        h5.className = `hidden`
+                editBtn.addEventListener('click', renderForm)
+
+                h5.className = `hidden`
+                p.append(h5)       
+                p.addEventListener('mouseover', () =>{
+                h5.className = ``
+                })
+                p.addEventListener('mouseout', () =>{
+                h5.className = `hidden`
+                })
+                resultsDiv.append(p)
+            }
         })
-        resultsDiv.append(p)
-        }
-    })
     })
 }
 
-// function renderForm () {
-// h5.innerHTML = ''
-// console.log('working')
-// }
-
-
-
-const getRandom = (arr) => {
-    return arr[Math.floor(Math.random() * arr.length)];
-};
-
+// Initiate search when search button is clicked
 document.querySelector('#search-btn').addEventListener('click', renderResults);
 
+// Initiate search when Enter key is pressed
 document.addEventListener('keydown', (e)=>{
 if(e.key === 'Enter'){
 renderResults()
 }
 });
 
+// Display random brewery
+const getRandom = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+};
 
 document.addEventListener(`DOMContentLoaded`, (e)=>{
     randomDiv.innerHTML =``
